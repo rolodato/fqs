a6fac6b397dd21e34b919adadb3fbbdd0a13a3d4
Rodrigo López Dato <rodrigo@auth0.com>
2016-09-18 07:00:51 -0300

    1.0.1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+#! /usr/bin/env node
+
 const fqs = require('./lib');
 const minimist = require('minimist');
-const result = fqs(minimist(process.argv.slice(1))._[0].toString());
+const result = fqs(minimist(process.argv.slice(2))._[0].toString());
 if (result) {
 	console.log(result);
 	process.exit(0);
