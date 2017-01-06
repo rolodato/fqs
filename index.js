850607ba4c4902757deb8572d50eebb002fd92ec
Rodrigo López Dato <rolodato@rolodato.com>
2017-01-06 00:22:40 -0300

    Remove unneeded dependencies

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 #! /usr/bin/env node
 
 const fqs = require('./lib');
-const minimist = require('minimist');
-const input = minimist(process.argv.slice(2))._[0] || '';
+const input = process.argv.slice(2)[0] || '';
 const result = fqs(input.toString());
 if (result) {
     console.log(result);
