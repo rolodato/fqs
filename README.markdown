ef3612ed155e360e888593b78f790d5ad7fae2b9
Rodrigo López Dato <rolodato@rolodato.com>
2017-01-06 00:50:41 -0300

    Add stdin support

--- README.markdown
+++ README.markdown
@@ -4,14 +4,23 @@ Pretty-print query strings and hash fragments in a human-readable, copy-pasteabl
 
 ## Installation
 
-```
+```sh
 npm install -g fqs
 ```
 
 ## Usage
 
-```
+```sh
 $ fqs "https://example.com/foo?d=3&c=2#b=1&a=0"
+https://example.com/foo
+?c=2
+d=3
+#a=0
+b=1
+```
+
+```sh
+$ echo "https://example.com/foo?d=3c=2#b=1&a=0" | fqs
 https://example.com/foo
 ?c=2
 d=3
--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 #! /usr/bin/env node
 
 const fqs = require('./lib');
-const input = process.argv.slice(2)[0] || '';
-const result = fqs(input.toString());
-if (result) {
-    console.log(result);
-    process.exit(0);
+var input = '';
+if (process.stdin.isTTY) {
+    pretty(process.argv[2] || '');
 } else {
-    process.exit(1);
+    process.stdin.setEncoding('utf-8');
+    process.stdin.on('readable', () => {
+        var chunk = process.stdin.read();
+        if (chunk !== null) {
+            input += chunk;
+        }
+    });
+    process.stdin.on('end', () => {
+        pretty(input);
+    });
+}
+
+function pretty (input) {
+    const result = fqs(input);
+    if (result) {
+        console.log(result);
+        process.exit(0);
+    } else {
+        process.exit(1);
+    }
 }
